ac2a3909d6e30fbfcbb0e4a9a3d0a4d292c7a217
程星 <71432113+starmemda@users.noreply.github.com>
2021-07-14 17:32:57 +0800

    Create extract.sh

---
+++ extract.sh
@@ -0,0 +1,5 @@
+python extrat_image_feature_from_image.py \
+    --imagelist_file your_image_file_list \
+    --out_path_root your_featurefile_save_path \
+    --modelname resnet_imagenet \
+    -b 48
